0456c0973b8109d8f2003c85f2e8d8ff77fbf557
ANSAM AL SHARIF <u1392547@umail.utah.edu>
2026-06-12 23:48:11 -0600

    Added more to dashboard page and starting to create my api to replace spoonacular. created db from usda to query for ingredients.

--- server.ts
+++ server.ts
@@ -1,6 +1,142 @@
 import 'dotenv/config';
-import express, { Express, Request, Response, NextFunction } from 'express';
+import express, { Express, Request, Response, NextFunction, response } from 'express';
 import cors from 'cors';
+import * as cheerio from 'cheerio';
+import { parseIngredient } from 'parse-ingredient';
+import pg from 'pg';
+
+// notes
+// conversions not found in usda database are taken from https://www.allrecipes.com/article/cup-to-gram-conversions/
+
+
+const { Pool } = pg
+const pool = new Pool({ connectionString: process.env.DATABASE_URL })
+
+function decodeIngred(string: string){
+  return string
+    .replace(/&#(\d+);/g, (_, code) => String.fromCharCode(code))
+    .replace(/&amp;/g, '&')
+    .replace(/&lt;/g, '<')
+    .replace(/&gt;/g, '>')
+    .replace(/&quot;/g, '"')
+    .replace(/&apos;/g, "'")
+}
+
+function cleanIngred(string: string){
+  const decoded = decodeIngred(string)
+  const weightMatch = decoded.match(/\(([^)]+(?:g|oz|lb|kg|ounce|gram|pound|mL|ml))[^)]*\)/i)
+  const weight = weightMatch ? weightMatch[1] : null
+
+  const cleaned = decoded
+    .replace(/\(.*?\)/g, '')   // now remove parentheses
+    .replace(/\band\b/gi, '')
+    .replace(/\s+/g, ' ')
+    .trim()
+
+  return { cleaned, weight }
+}
+
+async function parseRecipeUrl(url: string){
+  console.log('fetching');
+  return await fetch(url)
+    .then(response => response.text())
+    .then(html => {
+      console.log('parsing');
+      const $ = cheerio.load(html)
+  
+      const scriptTag = $('script[type="application/ld+json"]').html()
+      const data = JSON.parse(scriptTag as string)
+      
+      // Some sites nest it in a @graph array
+      const recipe = data['@type'] === 'Recipe' 
+        ? data 
+        : data['@graph']?.find((item: { [x: string]: string; }) => item['@type'] === 'Recipe');
+
+      console.log(recipe.recipeIngredient);
+      //const ingredients = convertIngredients(recipe.recipeIngredient);
+      const ingredients = ingredParser(recipe.recipeIngredient);
+      convertToG(ingredients[0].amount as number, ingredients[0].unit as string, ingredients[0].name);
+      const weightPerServing = { amount: recipe.recipeYield[1], unit: recipe.recipeYield[1]};
+      const obj = {
+        title:        recipe.name,
+        nutrition:    { ingredients, weightPerServing }, 
+        servings:     recipe.recipeYield[0],
+        prepTime:     recipe.prepTime,
+        cookTime:     recipe.cookTime
+      };
+      return obj;
+    })
+    .catch(function(error){
+      //return error
+      console.log(error);
+    });
+}
+
+function ingredParser(ingredients: string[]){
+  let formatted = [];
+  for (const ingred in ingredients){
+    const { cleaned, weight } = cleanIngred(ingredients[ingred]); // use weight for g calc later
+    const parsed = parseIngredient(cleaned)[0];
+    console.log(parsed);
+    const p = {
+      amount: parsed.quantity,
+      unit: parsed.unitOfMeasure,
+      name: parsed.description
+    }
+    formatted.push(p);
+  }
+  return formatted;
+}
+
+async function convertToG(amount:number, unit:string, ingred:string){
+  // if ingred = eggs, then unit is egg
+  // oil is in mililiters, add cup conversion
+  // ignore water, salt
+  // or descriptions take the first ingredient
+  
+  switch(unit){
+    case 'cups':
+    case 'cup':
+      const result = await pool.query(`SELECT elem->>'amount' as amount, elem->>'grams' as grams FROM food_with_portions, json_array_elements(portions) as elem WHERE to_tsvector('english', description) @@ websearch_to_tsquery('english', '${ingred}') AND elem->>'unit' = 'cup';`);
+      //console.log(result);
+      console.log(`${amount} cup/s is ${(result.rows[0].grams * amount) / result.rows[0].amount} in grams`);
+      break;
+    
+  }
+}
+
+function convertIngredients(ingredients: string[]){
+  let formatted = [];
+  for (const ingred in ingredients){
+    // 3 cases
+    // special characters like ⅔
+    // fractions like 1/2
+    // regular numbers
+    const amountMatch = ingredients[ingred].match(/^([^ ]+)/);
+    //console.log(amountMatch);
+    let amount = 'unknown';
+    if (amountMatch){
+      amount = amountMatch[0];
+    }
+    let unitMatch = ingredients[ingred].match(/(?:^|\s)([^ ]+)\s+([a-zA-Z]+)/);
+    let unit = 'unknown';
+    if (unitMatch){
+      unit = unitMatch[2];
+    }
+    let nameMatch = ingredients[ingred].match(/(?:^|\s)([^ ]+)\s+\w+\s+((?:\w+\s*){5})(?:\s*\(.*\))?/);
+    let name = 'unknown';
+    if (nameMatch){
+      name = nameMatch[2];
+    }
+    let obj = {
+      name: name,
+      amount: amount,
+      unit: unit
+    }
+    formatted.push(obj);
+  }
+  return formatted;
+}
 
 
 const app: Express = express();
@@ -9,11 +145,15 @@ app.use(cors());
 app.get('/api/extract', async (req, res) => {
   const { url } = req.query;
   
-  const response = await fetch(
-    `https://api.spoonacular.com/recipes/extract?url=${encodeURIComponent(url as string)}&includeNutrition=true&apiKey=${process.env.SPOONACULAR_KEY}`
-  );
-  const data = await response.json();
-  res.json(data);
+  // const response = await fetch(
+  //   `https://api.spoonacular.com/recipes/extract?url=${encodeURIComponent(url as string)}&includeNutrition=true&apiKey=${process.env.SPOONACULAR_KEY}`
+  // );
+  //console.log(url);
+  const response = await parseRecipeUrl(url as string);
+  // console.log('response 2 is ');
+  // console.log(response);
+  // const data = await response.json();
+  res.json(response);
 });
 
 app.listen(3001, () => console.log('Server running on port 3001'));
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { Button, Form, InputGroup, Container, Navbar, Nav } from 'react-bootstrap';
+import { Button, Card, Container, CardGroup } from 'react-bootstrap';
 import TopNav from '../components/navbar';
 import { useNavigate } from 'react-router-dom';
 
@@ -7,22 +7,37 @@ function Dashboard() {
     return (
         <>
             <TopNav/>
-            <Container className='d-flex justify-content-center align-items-center center-page w-100 ms-auto' style={{
-                flexDirection: 'row',
-                textAlign: 'left'}}>
-                <div className='' style={{display: 'flex', flexDirection: 'column'}}>
-                    <h1>Find the perfect recipes <span style={{color: '#CFB98C'}}>for you</span></h1>
-                    <p className=''>From adding your preferred diets, to adding a dietary restriction, this website has you covered! Start comparing recipes now by adding your personal preferences to hit your goals.</p>
-                    <Button className='styledBtn align-self-center p-3' onClick={()=> navigate('/')}>Get Started</Button>
-                    <div style={{display: 'flex', flexDirection: 'row', fontSize: '1.5rem', margin: '0 auto', gap: '35px', paddingTop: '1rem', textAlign: 'center'}}>
-                        <span><span style={{color: '#CFB98C'}}>10+</span><br/> Different Diets</span>
-                        <span>Compare up to <br/><span style={{ color: '323F27'}}>4</span><br/> recipes</span>
-                        <span><span style={{ color: '#546B41'}}>Any</span><br/> Combination</span>
+            <div className='w-100 min-vh-100' style={{backgroundColor: '#546B41'}}>
+                <Container className='d-flex justify-content-center align-items-center center-page w-50 p-5 ms-auto' style={{
+                    flexDirection: 'column',
+                    color: 'white',
+                    textAlign: 'center'}}>
+                    
+                    
+                    <div className='' style={{display: 'flex', flexDirection: 'column'}}>
+                        <h1>Find the perfect recipes <span style={{textDecoration: 'underline'}}>for you</span></h1>
+                        <p className=''>From adding your preferred diets, to adding a dietary restriction, this website has you covered! Start comparing recipes now by adding your personal preferences to hit your goals.</p>
+                        <Button className='styledBtn align-self-center p-3' onClick={()=> navigate('/')}>Get Started</Button>
+                        
                     </div>
-                </div>
-                <img src='apple.png' alt="ex" style={{width: '50%'}}/>
-                
-            </Container>
+                    
+                    
+                </Container>
+                <CardGroup style={{display: 'flex', flexDirection: 'row', gap: '35px', paddingTop: '1rem', textAlign: 'center'}} className='ms-auto p-5'>
+                        <Card className='p-2 rounded shadow'>
+                            <Card.Title><span style={{color: '#CFB98C'}}>10+</span> Different Diets</Card.Title>
+                            <Card.Text className='text-muted'>
+                                Ranging from sensitivities to medical diets, we include many different kinds of diets.
+                            </Card.Text>
+                        </Card>
+                        <Card>
+                            <Card.Title>Compare up to <br/><span style={{ color: '#323F27'}}>4</span><br/> recipes</Card.Title>
+                        </Card>
+                        <Card>
+                            <Card.Title><span style={{ color: '#546B41'}}>Any</span><br/> Combination</Card.Title>
+                        </Card>
+                </CardGroup>
+            </div>
         </>
     )
 
